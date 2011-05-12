cf69c7669cf40b568cd5253deb04fba9ea1118d5
jin <infinitus.internal@gmail.com>
2011-05-12 23:37:18 +0800

    initial commit

---
+++ level2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import math
+
+minNumber = 227000 
+fiboNew = 1
+fiboOld = 0 
+fiboPrime = False
+primeFactor = 2
+sumOfPrimeFactors = 0
+
+def isPrime(number):  
+    if number <= 1:  
+        return False  
+    divisor = 2
+    divisorMax = (int(math.sqrt(number)+1))
+    while divisor < divisorMax:  
+        if number % divisor == 0:  
+            return False  
+        divisor += 1  
+    return True
+
+while fiboNew < 100000000000:
+    fiboNum = fiboOld
+    if fiboNum > minNumber:
+        if isPrime(fiboNum) == True:
+			break;
+    fiboNum = fiboNew
+    fiboNew += fiboOld
+    fiboOld = fiboNum
+
+fiboNum += 1
+print fiboNum
+
+while fiboNum != 1:
+	if isPrime(primeFactor) == True:
+		while fiboNum % primeFactor == 0:
+			fiboNum = fiboNum / primeFactor
+			sumOfPrimeFactors += primeFactor
+			print ("/" + str(primeFactor) + "=" + str(fiboNum))
+	primeFactor += 1
+
+print("Sum of prime factors = " + str(sumOfPrimeFactors))
